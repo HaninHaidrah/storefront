9e0dd56ded33c4b7c71fea5bdfb8e8da9b7ed341
HaninHaidrah <haneenhadey@gmail.com>
2021-12-19 21:55:01 +0200

    lab36

---
+++ my-app/src/compononts/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+      <div className="header">
+          <h1 style={{background:"black" ,color:"white",textAlign:"center"}}>EASY STORY</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
